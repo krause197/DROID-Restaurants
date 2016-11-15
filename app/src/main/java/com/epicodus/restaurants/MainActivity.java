79cd55ad76f661cdce0a5a2f7ffc8cb8a96fd708
Eric Krause <krause197@gmail.com>
2016-11-15 14:27:06 -0800

    add toast to button click listener

--- app/src/main/java/com/epicodus/restaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/restaurants/MainActivity.java
@@ -2,12 +2,23 @@ package com.epicodus.restaurants;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaruantsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFindRestaruantsButton = (Button) findViewById(R.id.findRestaurandButton);
+            mFindRestaruantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                   Toast.makeText(MainActivity.this, "Welcome to PDX", Toast.LENGTH_LONG).show();
+                }
+            });
     }
 }
